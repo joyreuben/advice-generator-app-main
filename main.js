84bc13a4085ffef7cc2585075c7acb77cac56cb6
joyreuben <67860973+joyreuben@users.noreply.github.com>
2022-08-31 19:21:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const adviceNum = document.getElementById("advice-number");
+const adviceText = document.querySelector("p");
+const dice = document.getElementById("dice");
+  
+window.onload = showQuote;
+
+dice.addEventListener("click", function(){
+    showQuote();
+});
+
+function showQuote(){
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then((data) => data.slip)
+    .then((data) => {
+        adviceNum.textContent = data.id;
+        adviceText.innerHTML = data.advice;
+    })
+    .catch((error) => {
+        alert(`Error ${error}`);
+    });
+}
